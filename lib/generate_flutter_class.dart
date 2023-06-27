d7c8e3240994af89808733a1df60591a571c50f2
cyberpwn <danielmillst@gmail.com>
2023-06-27 10:23:48 -0400

    Icon generator

---
+++ lib/generate_flutter_class.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:icon_generator/templates/flutter_icons.dart' as template;
+import 'package:recase/recase.dart';
+
+class GenerateResult {
+  GenerateResult(this.content, this.iconsCount);
+
+  final String content;
+  final int iconsCount;
+}
+
+Future<GenerateResult> generateFlutterClass({
+  required File iconMap,
+  required String className,
+  required String? packageName,
+  required String namingStrategy,
+  String indent = '  ',
+}) async {
+  final Map<String, dynamic> icons = jsonDecode(await iconMap.readAsString());
+
+  final dartIconsEntries = icons.entries.map(
+    (entry) => someReplace(
+      template.icon
+          .replaceFirst(
+            '%ICON_NAME%',
+            namingStrategy == 'snake'
+                ? ReCase(entry.key).snakeCase
+                : ReCase(entry.key).camelCase,
+          )
+          .replaceFirst(
+              '%ICON_CODE%', entry.value.toRadixString(16).toString()),
+      className: className,
+      indent: indent,
+    ),
+  );
+
+  return GenerateResult(
+      someReplace(
+        template.base
+            .replaceFirst(
+              '%PACKAGE%',
+              packageName == null
+                  ? ''
+                  : someReplace(
+                      template.package
+                          .replaceFirst('%PACKAGE_NAME%', packageName),
+                      className: className,
+                      indent: indent,
+                    ),
+            )
+            .replaceFirst('%CONTENT%', dartIconsEntries.join('\n')),
+        className: className,
+        indent: indent,
+      ),
+      icons.length);
+}
+
+String someReplace(
+  String template, {
+  required String indent,
+  required String className,
+}) =>
+    template
+        .replaceAll('%INDENT%', indent)
+        .replaceAll('%CLASS_NAME%', className);
